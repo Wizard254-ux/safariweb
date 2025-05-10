83576acfcdfe78cc96305f3d80a35f04b612b06f
Wizard254-ux <brianndesa262@gmail.com>
2025-05-11 01:53:43 +0300

    vv

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Mountain, Compass, Sun, Palmtree, Camera } from 'lucide-react';
 
 const SafariSpinner = () => {
